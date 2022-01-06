9ece9e938845bf1615e1abadadacadb4b7595a6a
BearingMe <gomes.bruno201@gmail.com>
2022-01-06 06:27:06 -0300

    feat: adiciona a viewengine pug

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 // import libs
 import express from 'express';
+import path from 'path';
+import pug from 'pug';
 
 // import files
 import homeRouter from './routes/home';
@@ -9,6 +11,11 @@ import aboutRouter from './routes/about';
 const app = express();
 const port = process.env.PORT || 3000;
 
+// load view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// load routes 
 app.use('/', homeRouter);
 app.use('/about', aboutRouter);
 
--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get(/\/(home)?/, (req, res) => {
-  res.send('<h1>Home Page!</h1>')
+  res.render('home')
 })
 
 export default router
