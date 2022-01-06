0cd4db0d13b829d5aabf786bfec7b31951711ead
BearingMe <gomes.bruno201@gmail.com>
2022-01-06 07:32:33 -0300

    feat: adiciona um template para a about page

--- src/routes/about.ts
+++ src/routes/about.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('<h1>About Page!</h1>');
+  res.render('about');
 });
 
 export default router;
