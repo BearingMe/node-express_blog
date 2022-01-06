191b9c0f555b49794ab80dda9bcbd593fe50e549
BearingMe <gomes.bruno201@gmail.com>
2022-01-06 06:14:40 -0300

    feat: adiciona a rota para a aboutpage

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import express from 'express';
 
 // import files
 import homeRouter from './routes/home';
+import aboutRouter from './routes/about';
 
 // main program
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/', homeRouter);
+app.use('/about', aboutRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
