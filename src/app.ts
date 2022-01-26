2c9741fbd74eee6b4996533f6654a78fec5bdd0c
BearingMe <gomes.bruno201@gmail.com>
2022-01-25 22:04:46 -0300

    refactor: altera arquivos e configurações

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,37 @@
 // import libs
-import express from 'express';
-import session from 'express-session';
-import path from 'path';
+import express from "express";
+import session from "express-session";
+import path from "path";
 
 // import files
-import homeRouter from './routes/home';
-import aboutRouter from './routes/about';
-import registerRouter from './routes/register';
-import loginRouter from './routes/login';
+import homeRouter from "./routes/home";
+import aboutRouter from "./routes/about";
+import registerRouter from "./routes/register";
+import loginRouter from "./routes/login";
+import * as config from "./config";
 
-// constants 
+// constants
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
 // set up session
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(session(config.session));
 
 // load view engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // load body parser
 app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // load static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-// load routes 
-app.use('/', homeRouter);
-app.use('/about', aboutRouter);
-app.use('/register', registerRouter);
-app.use('/login', loginRouter);
+// load routes
+app.use("/", homeRouter);
+app.use("/about", aboutRouter);
+app.use("/register", registerRouter);
+app.use("/login", loginRouter);
 
 // start server
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const session = {
+  secret: process.env.SESSION_SECRET ?? "secret",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  },
+};
